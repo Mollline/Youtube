9329001b794d604190f3c5a220789701c3d3d9cd
Mollline <147712486+Mollline@users.noreply.github.com>
2023-10-12 18:32:27 +0800

    Add files via upload

---
+++ youtube/src/app/page.js
@@ -0,0 +1,17 @@
+
+import { Header } from "../Component/Header";
+import { Body } from "../Component/Body";
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        backgroundColor: "RGB(33,33,33)",
+      }}
+    >
+      <Header />
+      <Body />
+      <footer></footer>
+    </div>
+  );
+}
